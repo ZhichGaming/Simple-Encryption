5e63a60eeb78ae53be0640b4279e35b1bb2282a0
ZhichGaming <nick.zhicheng@gmail.com>
2022-09-07 14:29:34 -0400

    Initial Commit

---
+++ Simple Encryption/Simple_EncryptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_EncryptionApp.swift
+//  Simple Encryption
+//
+//  Created by Nick on 2022-09-07.
+//
+
+import SwiftUI
+
+@main
+struct Simple_EncryptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
